46648ca676d24694dc6caa115183d4c312f549d1
Doug Farrell <doug.farrell@gmail.com>
2020-08-27 19:06:58 -0400

    Making 01.py clearer and more concise

--- examples/CH_05/examples/01.py
+++ examples/CH_05/examples/01.py
@@ -6,23 +6,26 @@ from time import sleep, time
 
 def timing_decorator(func):
     @functools.wraps(func)
-    def wrapper():
+    def wrapper(delay):
         start_time = time()
         print("starting timing")
-        result = func()
+        result = func(delay)
         print(f"task elapsed time: {time() - start_time}")
         return result
     return wrapper
 
 
 @timing_decorator
-def complex_task():
-    delay = uniform(0.25, 3)
+def complex_task(delay):
     sleep(delay)
     return "task done"
 
 
 def main():
-    print("running")
-    for _ in range(5):
-        print(complex_task())
+    for _ in range(3):
+        delay = uniform(0.25, 3)
+        print(complex_task(delay))
+
+
+if __name__ == "__main__":
+    main()
