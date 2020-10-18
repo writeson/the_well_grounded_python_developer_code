a19ce8923e1b565b30514dfa38f00aea5de77884
Doug Farrell <doug.farrell@gmail.com>
2020-10-18 18:28:15 -0400

    CH_07 - updated the code to streamline some things and remove unnecessary complexities

--- examples/CH_07/examples/03/static/js/index.js
+++ examples/CH_07/examples/03/static/js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function (event) {
     let banner = document.querySelector("#header h1");
     window.addEventListener('click', function (event) {
-        // is this the click even we're looking for?
+        // is this the click event we're looking for?
         if (event.target.matches('.change-banner-color')) {
             let color = banner_colors[Math.floor(Math.random() * banner_colors.length)];
             banner.style.backgroundColor = color;
